d6644b7e3377f7db19e644fc9927ce22dd9d0dfb
zsh2401 <zsh2401@163.com>
2017-06-08 19:01:07 +0800

    UI最佳实践

--- UIBestPractice/app/src/main/java/top/zsh2401/uibestpractice/MainActivity.java
+++ UIBestPractice/app/src/main/java/top/zsh2401/uibestpractice/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private Button send;
     private RecyclerView msgRecy;
     private MsgAdapter adapter;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
